47103f8755839ffb9246883013b3dfb8a2ca5466
Saurav Dhyani <sdhyani@archerpoint.com>
2023-07-11 02:41:27 +0530

    Steps BC14 To BC 21.

---
+++ BC14-To-BC21/BC14-BC21-Step1.ps1
@@ -0,0 +1,14 @@
+﻿#Import Module
+Import-Module 'C:\Program Files\Microsoft Dynamics 365 Business Central\140\Service\NavAdminTool.ps1'
+
+#Uninstall Extensions from BC 14
+Get-NAVAppInfo -ServerInstance BC140 -Tenant default | % { Uninstall-NAVApp -ServerInstance BC140 -Tenant default -Name $_.Name -Version $_.Version -Force}
+
+#Unpublish Extensions from BC 14
+Get-NAVAppInfo -ServerInstance BC140 | % { Unpublish-NAVApp -ServerInstance BC140 -Name $_.Name -Version $_.Version }
+
+#Remove Symbols
+Get-NAVAppInfo -ServerInstance BC140 -SymbolsOnly | % { Unpublish-NAVApp -ServerInstance BC140 -Name $_.Name -Version $_.Version }
+
+#Stop Service
+Stop-NAVServerInstance -ServerInstance BC140
