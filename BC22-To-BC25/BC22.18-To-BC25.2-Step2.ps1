3e40506634b9e8068670731d45cc2a7a7d127277
Saurav Dhyani <postsaurav@gmail.com>
2025-11-19 15:35:29 +0530

    Updated Repo To Correct Folders

---
+++ BC22-To-BC25/BC22.18-To-BC25.2-Step2.ps1
@@ -0,0 +1,74 @@
+﻿########################################################################
+#Author : Saurav Dhyani
+#Date   : 13-01-2025
+#Purpose: Execute Step 2 of BC Upgrade
+########################################################################
+
+
+#Import Module
+Import-Module 'C:\Program Files\Microsoft Dynamics 365 Business Central\252\Service\NavAdminTool.ps1'
+
+#Convert the application database to version 25
+Invoke-NAVApplicationDatabaseConversion -DatabaseServer localhost -DatabaseName BC22_To_BC25
+
+#Configure version 25 server for DestinationAppsForMigration
+Set-NAVServerConfiguration -ServerInstance BC252 -KeyName DatabaseServer -KeyValue localhost
+#Set-NAVServerConfiguration -ServerInstance BC252 -KeyName DatabaseInstance -KeyValue 
+Set-NAVServerConfiguration -ServerInstance BC252 -KeyName DatabaseName -KeyValue BC22_To_BC25
+Set-NavServerConfiguration -ServerInstance BC252 -KeyName "EnableTaskScheduler" -KeyValue false
+
+Restart-NAVServerInstance -ServerInstance BC252
+
+#Import-NAVServerLicense -ServerInstance BC252 -LicenseFile "C:\Upgrade\Dev.bclicense"
+#Restart-NAVServerInstance -ServerInstance BC252
+
+#Synchronize tenant
+Sync-NAVTenant -ServerInstance BC252 -Tenant default -Mode Sync
+
+#Publish extensions
+Publish-NAVApp -ServerInstance BC252 -Path "E:\Upgrade To BC 25 - DE\Core Apps\Microsoft_System Application.app"
+Publish-NAVApp -ServerInstance BC252 -Path "E:\Upgrade To BC 25 - DE\Core Apps\Microsoft_Business Foundation.app"
+Publish-NAVApp -ServerInstance BC252 -Path "E:\Upgrade To BC 25 - DE\Core Apps\Microsoft_Base Application.app"
+Publish-NAVApp -ServerInstance BC252 -Path "E:\Upgrade To BC 25 - DE\Core Apps\Microsoft_Application.app"
+
+#Restart service
+Restart-NAVServerInstance -ServerInstance BC252
+
+
+#Synchronize Apps
+Sync-NAVApp -ServerInstance BC252 -Tenant default -Name "System Application" -Version 25.2.27733.27999
+Sync-NAVApp -ServerInstance BC252 -Tenant default -Name "Business Foundation" -Version 25.2.27733.27999
+Sync-NAVApp -ServerInstance BC252 -Tenant default -Name "Base Application" -Version 25.2.27733.27999
+Sync-NAVApp -ServerInstance BC252 -Tenant default -Name "Application" -Version 25.2.27733.27999
+
+
+#Upgrade data
+Start-NAVAppDataUpgrade -ServerInstance BC252 -Name "System Application" -Version 25.2.27733.27999
+Install-NAVApp -ServerInstance BC252 -Name "Business Foundation" -Version 25.2.27733.27999
+Start-NAVAppDataUpgrade -ServerInstance BC252 -Name "Base Application" -Version 25.2.27733.27999
+Start-NAVAppDataUpgrade -ServerInstance BC252 -Name "Application" -Version 25.2.27733.27999
+
+#Run Data Upgrade
+Start-NAVDataUpgrade -ServerInstance BC252 -Tenant default -FunctionExecutionMode Serial -SkipAppVersionCheck
+Get-NAVDataUpgrade -ServerInstance BC252 -Progress
+
+#Upgrade control add-ins
+$AddinsFolder = 'C:\Program Files\Microsoft Dynamics 365 Business Central\252\Service\Add-ins'
+
+Set-NAVAddIn -ServerInstance BC252 -AddinName 'Microsoft.Dynamics.Nav.Client.BusinessChart' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $AddinsFolder 'BusinessChart\Microsoft.Dynamics.Nav.Client.BusinessChart.zip')
+Set-NAVAddIn -ServerInstance BC252 -AddinName 'Microsoft.Dynamics.Nav.Client.FlowIntegration' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $AddinsFolder 'FlowIntegration\Microsoft.Dynamics.Nav.Client.FlowIntegration.zip')
+Set-NAVAddIn -ServerInstance BC252 -AddinName 'Microsoft.Dynamics.Nav.Client.OAuthIntegration' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $AddinsFolder 'OAuthIntegration\Microsoft.Dynamics.Nav.Client.OAuthIntegration.zip')
+Set-NAVAddIn -ServerInstance BC252 -AddinName 'Microsoft.Dynamics.Nav.Client.PageReady' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $AddinsFolder 'PageReady\Microsoft.Dynamics.Nav.Client.PageReady.zip')
+Set-NAVAddIn -ServerInstance BC252 -AddinName 'Microsoft.Dynamics.Nav.Client.PowerBIManagement' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $AddinsFolder 'PowerBIManagement\Microsoft.Dynamics.Nav.Client.PowerBIManagement.zip')
+Set-NAVAddIn -ServerInstance BC252 -AddinName 'Microsoft.Dynamics.Nav.Client.RoleCenterSelector' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $AddinsFolder 'RoleCenterSelector\Microsoft.Dynamics.Nav.Client.RoleCenterSelector.zip')
+Set-NAVAddIn -ServerInstance BC252 -AddinName 'Microsoft.Dynamics.Nav.Client.SatisfactionSurvey' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $AddinsFolder 'SatisfactionSurvey\Microsoft.Dynamics.Nav.Client.SatisfactionSurvey.zip')
+Set-NAVAddIn -ServerInstance BC252 -AddinName 'Microsoft.Dynamics.Nav.Client.VideoPlayer' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $AddinsFolder 'VideoPlayer\Microsoft.Dynamics.Nav.Client.VideoPlayer.zip')
+Set-NAVAddIn -ServerInstance BC252 -AddinName 'Microsoft.Dynamics.Nav.Client.WebPageViewer' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $AddinsFolder 'WebPageViewer\Microsoft.Dynamics.Nav.Client.WebPageViewer.zip')
+Set-NAVAddIn -ServerInstance BC252 -AddinName 'Microsoft.Dynamics.Nav.Client.WelcomeWizard' -PublicKeyToken 31bf3856ad364e35 -ResourceFile ($AppName = Join-Path $AddinsFolder 'WelcomeWizard\Microsoft.Dynamics.Nav.Client.WelcomeWizard.zip')
+
+
+#Change application version
+Set-NAVApplication -ServerInstance BC252 -ApplicationVersion 25.2.27733.27999 -Force
+Sync-NAVTenant -ServerInstance BC252 -Mode Sync -Tenant default
+Start-NAVDataUpgrade -ServerInstance BC252 -FunctionExecutionMode Serial -Tenant default -SkipUserSessionCheck
+Set-NAVServerConfiguration -ServerInstance BC252 -KeyName SolutionVersionExtension -KeyValue "437dbf0e-84ff-417a-965d-ed2bb9650972" -ApplyTo All  
--- README.md
+++ README.md
@@ -1,14 +1,71 @@
 # DataUpgrade
-This Repo will contain Data Upgrade Scripts.
+This Repo will contain Data Upgrade Scripts and Links for the video.
+
+### Data Upgrade NAV 2016 ➡️ BC 26 IN | India Localization | Ultimate Guide (Part 5)
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/Rng_DsoP5dM/0.jpg)](https://www.youtube.com/watch?v=Rng_DsoP5dM)
+
+### Data Upgrade NAV 2016 ➡️ BC 26 IN | India Localization | Ultimate Guide (Part 4)
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/oUKa_ETtqkY/0.jpg)](https://www.youtube.com/watch?v=oUKa_ETtqkY)
+
+### Data Upgrade NAV 2016 ➡️ BC 26 IN | India Localization | Ultimate Guide (Part 3)
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/EiCDCZ2D5W8/0.jpg)](https://www.youtube.com/watch?v=EiCDCZ2D5W8)
+
+### Data Upgrade NAV 2016 ➡️ BC 26 IN | India Localization | Ultimate Guide (Part 2)
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/SBDxyd4VRSQ/0.jpg)](https://www.youtube.com/watch?v=SBDxyd4VRSQ)
+
+### Data Upgrade NAV 2016 ➡️ BC 26 IN | India Localization | Ultimate Guide (Part 1) 
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/OSkNmGuShQk/0.jpg)](https://www.youtube.com/watch?v=OSkNmGuShQk)
+
+### 🚀 Upgrade to Business Central 25 from BC 19 – Step-by-Step Guide {IN}
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/hqCBsKheMi4/0.jpg)](https://www.youtube.com/watch?v=hqCBsKheMi4) 
+
+### Data Replication in Business Central What You Need to Know Before It’s Gone!
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/taybX8w4Nl0/0.jpg)](https://www.youtube.com/watch?v=taybX8w4Nl0) 
+
+### Upgrade Data from Dynamics BC 20 to BC 25 Step by Step Guide {AU}
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/4E2hWvIVcBU/0.jpg)](https://www.youtube.com/watch?v=4E2hWvIVcBU) 
+
+### Upgrade Data from Dynamics BC 21 to BC 25 Step by Step Guide {CA}
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/1T_53R96quo/0.jpg)](https://www.youtube.com/watch?v=1T_53R96quo)
+
+### Upgrade Data from Dynamics BC 22 to BC 25 Step by Step Guide {DE}
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/8nROvFcZGjY/0.jpg)](https://www.youtube.com/watch?v=8nROvFcZGjY)
+
+### Upgrade Data from Dynamics BC 23 to BC 25 {GB}
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/TvlZDMFuHI8/0.jpg)](https://www.youtube.com/watch?v=TvlZDMFuHI8)
+
+### Data Upgrade from Business Central 24 to Business Central 25.
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/4DIDI6niiVE/0.jpg)](https://www.youtube.com/watch?v=4DIDI6niiVE)
 
 ### Data Upgrade from Business Central 14 with Customization to Business Central 24.
 [![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/r7S11ST4PSg/0.jpg)](https://www.youtube.com/watch?v=r7S11ST4PSg)
 
+### Replicate Data from On-Premises to BC SaaS Different Version!
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/XRLN_3Y25cU/0.jpg)](https://www.youtube.com/watch?v=XRLN_3Y25cU)
+
 ### Data Upgrade to Business Central 2024 Release Wave 1.
 [![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/J6O6rzUjE4Y/0.jpg)](https://www.youtube.com/watch?v=J6O6rzUjE4Y)
 
+### Breaking New Ground: The First-Ever Jump Version of Business Central.
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/3i9SPWbzMlA/0.jpg)](https://www.youtube.com/watch?v=3i9SPWbzMlA)
+
+### Upgrading Your Customized C/AL Application to Business Central 23 - Part 2
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/VkFMolkL7JA/0.jpg)](https://www.youtube.com/watch?v=VkFMolkL7JA)
+
+### Upgrading Your Customized C/AL Application to Business Central 23 - Part 1
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/uQ9Dva7Dpxg/0.jpg)](https://www.youtube.com/watch?v=uQ9Dva7Dpxg)
+
+### Seamless Transition: Convert C/AL Customization to AL Extension with txt2al.
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/pK085U1ntpM/0.jpg)](https://www.youtube.com/watch?v=pK085U1ntpM)
+
+### Data Upgrade from NAV 2016 to Business Central 14 C/AL.
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/PCOdKT8ninc/0.jpg)](https://www.youtube.com/watch?v=PCOdKT8ninc)
+
 ### Data Upgrade from Business Central 14 to Business Central 2023 using Customer License.
 [![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/-tfpIH-Ygg8/0.jpg)](https://www.youtube.com/watch?v=-tfpIH-Ygg8)
+
+### Data Replication from On-Premises to BC SaaS - Same Version!
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/6WrbjeFdzwA/0.jpg)](https://www.youtube.com/watch?v=6WrbjeFdzwA)
 
 ### Data Upgrade - Business Central 14 To Business Central 22.
 [![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/q95ShXoPIAQ/0.jpg)](https://www.youtube.com/watch?v=q95ShXoPIAQ)
