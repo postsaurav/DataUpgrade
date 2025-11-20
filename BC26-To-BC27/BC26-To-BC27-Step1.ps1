e34116b9f9d01f5ddc8c13e0e2f8648c359055c3
Saurav Dhyani <postsaurav@gmail.com>
2025-11-20 20:31:50 +0530

    Add V26 To V27

---
+++ BC26-To-BC27/BC26-To-BC27-Step1.ps1
@@ -0,0 +1,22 @@
+﻿﻿﻿########################################################################
+#Author : Saurav Dhyani
+#Date   : 19-11-2025
+#Purpose: Execute Step 1 of BC Upgrade
+#Steps  : 
+#1. Replace BC260 Parameter Below with your Old BC Server Instance Name (Service Name).
+########################################################################
+
+#Import Module
+Import-Module 'C:\Program Files\Microsoft Dynamics 365 Business Central\260\Service\NavAdminTool.ps1'
+
+#Uninstall Extensions from BC 26
+Get-NAVAppInfo -ServerInstance BC260 -Tenant default | % { Uninstall-NAVApp -ServerInstance BC260 -Tenant default -Name $_.Name -Version $_.Version -Force}
+
+#Unpublish Extensions from BC 26
+Get-NAVAppInfo -ServerInstance BC260 | % { Unpublish-NAVApp -ServerInstance BC260 -Name $_.Name -Version $_.Version}
+
+#Remove Symbols
+Get-NAVAppInfo -ServerInstance BC260 -SymbolsOnly | % { Unpublish-NAVApp -ServerInstance BC260 -Name $_.Name -Version $_.Version }
+
+#Stop Service
+Stop-NAVServerInstance -ServerInstance BC260
